8a4c111fdd1e9a6ba1b7ea6fecb675075932c2ed
TEAM VOIDZ <109448619+team-voidz@users.noreply.github.com>
2023-03-10 15:25:10 +0800

    Add files via upload

--- Clean_test.bat
+++ Clean_test.bat
@@ -743,40 +743,13 @@ echo	 zip files and temp folders DELETED!
 echo.
 echo Press any button for exit
 pause>nul 2>&1
-exit
+call DISS_delete.bat
 
 :delete3
 cls
 if exist "%~dp0\DISS" (RD /s /q "%~dp0\DISS")
 if exist "%~dp0\DISS_A" (RD /S /Q "%~dp0\DISS_A\")
-COLOR 02
-:::9      DDDDDDDDDDDDD      IIIIIIIIII   SSSSSSSSSSSSSSS    SSSSSSSSSSSSSSS 
-:::9      D::::::::::::DDD   I::::::::I SS:::::::::::::::S SS:::::::::::::::S
-:::9      D:::::::::::::::DD I::::::::IS:::::SSSSSS::::::SS:::::SSSSSS::::::S
-:::9      DDD:::::DDDDD:::::DII::::::IIS:::::S     SSSSSSSS:::::S     SSSSSSS
-:::9         D:::::D    D:::::D I::::I  S:::::S            S:::::S            
-:::9         D:::::D     D:::::DI::::I  S:::::S            S:::::S            
-:::9         D:::::D     D:::::DI::::I   S::::SSSS          S::::SSSS         
-:::9         D:::::D     D:::::DI::::I    SS::::::SSSSS      SS::::::SSSSS    
-:::9         D:::::D     D:::::DI::::I      SSS::::::::SS      SSS::::::::SS  
-:::9         D:::::D     D:::::DI::::I         SSSSSS::::S        SSSSSS::::S 
-:::9         D:::::D     D:::::DI::::I              S:::::S            S:::::S
-:::9         D:::::D    D:::::D I::::I              S:::::S            S:::::S
-:::9      DDD:::::DDDDD:::::DII::::::IISSSSSSS     S:::::SSSSSSSS     S:::::S
-:::9      D:::::::::::::::DD I::::::::IS::::::SSSSSS:::::SS::::::SSSSSS:::::S
-:::9      D::::::::::::DDD   I::::::::IS:::::::::::::::SS S:::::::::::::::SS 
-:::9       DDDDDDDDDDDDD      IIIIIIIIII SSSSSSSSSSSSSSS    SSSSSSSSSSSSSSS   
-for /f "delims=:::9 tokens=*" %%A in ('findstr /b :::9 "%~f0"') do @echo(%%A
-echo.
-echo	                                 DONE! and DONE!
-echo	 Please backup DISS_OLDSD.zip (old sd files) and DISS_ABC.zip (zip files backup)
-echo	                   Also we have backup it up at C:/dissbackup
-echo.
-echo	                    Put The SD back in your switch and boot
-echo.
-echo Press any button for exit
-pause>nul 2>&1
-exit
+call DISS_delete.bat
 
 :ENDgood
 COLOR 0f
