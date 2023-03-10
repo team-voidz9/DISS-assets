133cd577fa2caa1ce0371d3289fd50819b0fc09d
TEAM VOIDZ <109448619+team-voidz@users.noreply.github.com>
2023-03-10 16:13:10 +0800

    Add files via upload
    robocopy :

--- Clean_test.bat
+++ Clean_test.bat
@@ -516,7 +516,7 @@ echo ------------------------------------------------------------------------
 echo.
 TIMEOUT /T 3
 
-robocopy %sd%:\ %~dp0\DISS_B\ /E /COPYALL /PURGE /XD %sd%:\emuMMC %sd%:\backup %sd%:\games %sd%:\DISS %sd%:\DISS_A %sd%:\DISS_B %sd%:\Firmware /XF %sd%:\DISS_Delete.bat %sd%:\DISS_Downloader.bat %sd%:\Clean_clean.bat %sd%:\DISS_OLDSD.zip %sd%:\DISS_ABC.zip
+robocopy %sd%:\ %~dp0:\DISS_B\ /E /COPYALL /PURGE /XD %sd%:\emuMMC %sd%:\backup %sd%:\games %sd%:\DISS %sd%:\DISS_A %sd%:\DISS_B %sd%:\Firmware /XF %sd%:\DISS_Delete.bat %sd%:\DISS_Downloader.bat %sd%:\Clean_clean.bat %sd%:\DISS_OLDSD.zip %sd%:\DISS_ABC.zip
 
 powershell -command "Compress-Archive -Path %~dp0/DISS_B/ -Destination %~dp0/DISS_OLDSD.zip" -verbose -Force
 echo.
