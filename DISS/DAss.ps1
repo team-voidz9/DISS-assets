5f0c445a003bff66340634962d2e754a08233d44
TEAM VOIDZ <109448619+team-voidz@users.noreply.github.com>
2023-06-25 12:02:40 +0800

    -

--- DISS/DAss.ps1
+++ DISS/DAss.ps1
@@ -2,29 +2,23 @@ Write-Host "Asset Downloader Rev.1" -ForegroundColor green -BackgroundColor blac
 Write-Host "TEAM-VOIDZ"
 Write-Host "Latest : -"
 
-$repo = "team-voidz/DISS-assets"
-$filenamePattern = "assets_v*.zip"
+$repo1 = "team-voidz/DISS-assets"
+$filenamePattern1 = "assets_v*.zip"
 $innerDirectory = $true
 $preRelease = $true
 
+write-host -back Red "Downloading Assets"
+Write-Host "1. DISS Assets" -ForegroundColor green -BackgroundColor black
 if ($preRelease) {
-    $releasesUri = "https://api.github.com/repos/$repo/releases"
-    $downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri)[0].assets | Where-Object name -like $filenamePattern ).browser_download_url
+    $releasesUri1 = "https://api.github.com/repos/$repo1/releases"
+    $downloadUri1 = ((Invoke-RestMethod -Method GET -Uri $releasesUri1)[0].assets | Where-Object name -like $filenamePattern1 ).browser_download_url
 }
 else {
-    $releasesUri = "https://api.github.com/repos/$repo/releases/latest"
-    $downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri).assets | Where-Object name -like $filenamePattern ).browser_download_url
+    $releasesUri1 = "https://api.github.com/repos/$repo1/releases/latest"
+    $downloadUri1 = ((Invoke-RestMethod -Method GET -Uri $releasesUri1).assets | Where-Object name -like $filenamePattern1 ).browser_download_url
 }
-
-#  Casting to UriBuilder will split the URI into separate properties. Helpful for isolating path, query string, fragment, etc.
-$downloadUriBuilder = [System.UriBuilder] $downloadUri
-
-# Split for the last item in the URI Path 
-# Alternative: [System.IO.Path]::GetFileName($downloadUriBuilder.Path)
-$filename = Split-Path -Path ($downloadUriBuilder.Path) -Leaf       
-
-# (Optional) Decode any URL-encoded characters
-$filenameUrlDecoded = [System.Web.HttpUtility]::UrlDecode($filename)
-
-Invoke-WebRequest -Uri $downloadUri -Out $filenameUrlDecoded
+$downloadUriBuilder1 = [System.UriBuilder] $downloadUri1
+$filename1 = Split-Path -Path ($downloadUriBuilder1.Path) -Leaf       
+$filenameUrlDecoded1 = [System.Web.HttpUtility]::UrlDecode($filename1)
+Invoke-WebRequest -Uri $downloadUri1 -Out $filenameUrlDecoded1
 write-host -back Red "Latest DISS Assets Downloaded"
